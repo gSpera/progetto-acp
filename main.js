7f76d63ccf553638607974ebac6d410d94aaef77
Giovanni Spera <giovanni.spera2011@libero.it>
2023-06-28 12:30:09 +0200

    Modifiche fatte 27/06
    Fare commit per bene?? Tss, troppo facile, meglio così

--- main.js
+++ main.js
@@ -1,29 +1,53 @@
 const express = require('express')
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://127.0.0.1:27017/")
+
+const viaggioSchema = new mongoose.Schema({
+    id: Number,
+    partenza: String,
+    arrivo: String,
+    data: Number, // usiamo il timestamp unix per comodità, in millisecondi
+    durata: Number,
+    npostiPasseggeri: Number,
+    npostiVeicoli: Number,
+})
+const Viaggio = mongoose.model('Viaggio', viaggioSchema)
+
 const app = express()
 
 app.use(express.static('static'))
 
 app.get("/api/viaggi", (req, res) => {
-    res.json([
-        {
-            "data": "ieri",
-            "orario": "undici",
-            "partenza": "castellammare di stabia",
-            "arrivo": "sorrento",
-            "durata": "troppo",
-            "npostiPasseggeri": 10,
-            "npostiVeicoli": 11,
-        }, {
-            "data": "domani",
-            "orario": "presto",
-            "partenza": "sorrento",
-            "arrivo": "napoli",
-            "durata": "poco",
-            "npostiPasseggeri": 69,
-            "npostiVeicoli": 420,
-        }, {
-        }, {
-        }])
+    const unGiorno = 24 * 60 * 60 * 1000 // Durata di un giorno, in millisecondi
+    const partenza = req.query.partenza
+    const arrivo = req.query.arrivo
+    const data = Math.round(new Date(req.query.data).getTime() / unGiorno) * unGiorno // Timestamp
+    const npostiPasseggeri = Number(req.query.npostiPasseggeri) || 0
+    const npostiVeicoli = Number(req.query.npostiVeicoli) || 0
+
+    let search = {}
+    if (partenza) search["partenza"] = { $regex: String(partenza), $options: "i" }
+    if (arrivo) search["arrivo"] = { $regex: String(arrivo), $options: "i" }
+    if (data) search["data"] = { $gte: data, $lt: data + unGiorno }
+    search["npostiPasseggeri"] = { $gte: npostiPasseggeri }
+    search["npostiVeicoli"] = { $gte: npostiVeicoli }
+
+    // console.log(search)
+
+    Viaggio.find(search).exec()
+        .then(found => res.send(found))
+        .catch(err => console.error("Errore: " + err))
+})
+
+app.get("/api/autocomplete-partenza", (req, res) => {
+    Viaggio.find({}, "partenza")
+        .then(found => res.send(found.map(v => v.partenza)))
+        .catch(err => console.error("Errore: " + err))
+})
+app.get("/api/autocomplete-arrivo", (req, res) => {
+    Viaggio.find({}, "arrivo")
+        .then(found => res.send(found.map(v => v.arrivo)))
+        .catch(err => console.error("Errore: " + err))
 })
 
 app.listen(8080, () => {
--- static/main.js
+++ static/main.js
@@ -24,17 +24,45 @@ function aggiornaViaggi() {
 
 function creaViaggio(viaggio) {
     const el = templateViaggio.clone()
-    el.attr("id", "") // Rimuoviamo il tag id
-    el.find(".template-viaggio-data").text(viaggio.data)
-    el.find(".template-viaggio-orario").text(viaggio.orario)
+    el.attr("id", "") // Rimuoviamo l'attributo id
+    el.find(".template-viaggio-data").text(new Date(viaggio.data).toLocaleDateString('it-IT', { "dateStyle": "full" }))
+    el.find(".template-viaggio-orario").text(new Date(viaggio.data).toLocaleTimeString('it-IT'))
     el.find(".template-viaggio-partenza").text(viaggio.partenza)
     el.find(".template-viaggio-arrivo").text(viaggio.arrivo)
     el.find(".template-viaggio-durata").text(viaggio.durata)
     el.find(".template-viaggio-nposti-passeggeri").text(viaggio.npostiPasseggeri)
     el.find(".template-viaggio-nposti-veicoli").text(viaggio.npostiVeicoli)
+    el.find(".template-viaggio-acquista").on("click", e => popupAcquisto(viaggio))
     return el
+}
+
+function popupAcquisto(viaggio) {
+    $("#compra-biglietto").removeClass("hidden")
+    $("body").addClass("no-scrollbar")
+    $("#compra-biglietto-codice-viaggio").text(viaggio.id)
+    $("#compra-biglietto-data").text(new Date(viaggio.data).toLocaleString('it-IT'))
+    $("#compra-biglietto-partenza").text(viaggio.partenza)
+    $("#compra-biglietto-arrivo").text(viaggio.arrivo)
+    $("#compra-biglietto-npasseggeri").val(fieldPasseggeri.val())
+    $("#compra-biglietto-nveicoli").val(fieldVeicoli.val())
+}
+function popupAcquistoChiudi() {
+    $("#compra-biglietto").addClass("hidden")
+    $("body").removeClass("no-scrollbar")
 }
 
 $(".ricerca-viaggio-filtro").on("input", aggiornaViaggi)
 
 aggiornaViaggi()
+
+// Carichiamo i valori di autocomplete
+fetch("/api/autocomplete-partenza")
+    .then(r => r.json())
+    .then(autocompletes => autocompletes.forEach(partenza => {
+        $("#filtra-partenza-autocomplete").append($("<option />").attr("value", partenza))
+    }))
+fetch("/api/autocomplete-arrivo")
+    .then(r => r.json())
+    .then(autocompletes => autocompletes.forEach(arrivo => {
+        $("#filtra-arrivo-autocomplete").append($("<option />").attr("value", arrivo))
+    }))
